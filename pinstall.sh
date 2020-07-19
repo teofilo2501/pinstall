e649a54662555ae29fae85dccbed5e9c4a5fa224
Teófilo <teofilo@localhost.localdomain>
2020-07-19 18:18:29 -0300

    add sublime-text

--- pinstall.sh
+++ pinstall.sh
@@ -24,9 +24,13 @@ wget https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm -O tea
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
+#add sublimetext
+rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+
 #update
 dnf check-update
 dnf update
 
 #install packages
-dnf install mc nmap wine megasync nautilus-megasync anydesk google-chrome-stable teamviewer.rpm kodi remmina remmina-plugins-rdp transmission-gtk code -y
+dnf install mc nmap wine megasync nautilus-megasync anydesk google-chrome-stable teamviewer.rpm kodi remmina remmina-plugins-rdp transmission-gtk code sublime-text -y
